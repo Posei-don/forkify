76f72dd6e695bdc4a7b6ef8ceb2ed21bc84c1180
Posei-don <Omar.Japaridze@gmail.com>
2023-04-04 14:29:45 +0400

    Initial commit

---
+++ src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const KEY = 'c160bf91-e5d9-4348-bc46-0fd968c42152';
+export const MODAL_CLOSE_SEC = 2.5;
